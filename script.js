3d3546ce16276886b25995a45d43b1ed688fd645
Husayn <khaybabz@gmail.com>
2024-12-15 15:21:57 +0100

    Values, variables and naming convention

---
+++ script.js
@@ -0,0 +1,23 @@
+// This is an external JS file because it is located outside the html file
+let js = "amazing";
+// if (js === "amazing") alert("JavaScript is FUN!");
+
+// 40 + 8 + 23 - 10;
+// Consoe.log instructs the computer to display our code in the developer console in the browser.
+console.log(40 + 8 + 23 - 10);
+console.log("Husayn");
+console.log("$1000000000");
+
+// variable and value
+let firstname = "Sponge Bob";
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+
+// Naming convention
+let dolly_parton = "DP";
+let $money = 100000;
+let person = "Husayn";
+let PI = 3.141;
+let myFirstjob = "customer service";
+let mySecondJob = "software developer";
